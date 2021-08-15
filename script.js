36496d85f1da4d280cbe3a6293ed6123741c9e60
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-08-15 22:34:32 +0530

    complete netflix clone

---
+++ script.js
@@ -0,0 +1,36 @@
+// var container = [7, 1, 2, 3, 1];
+
+// // area = min(a, b) * (bi - ai)
+
+// const getMaxValue = function(heights){
+// 	for(var i = 0; i<heights.length;i++){
+//         var a = 0;
+//         var b = 1;
+//         var minValue = Math.min(heights[a], heights[b]);
+//         for(var j =0; j<heights.length;j++){
+//             var area = minValue;
+//         }
+//     }
+// }
+
+// getMaxValue(container);
+
+//console.time('running time')
+
+// npm install prompt-sync 
+//const prompt = require("prompt-sync")({sigint: true}); //package to take user input through terminal
+//const ps = promptSync({sigint: true}); //intrupt the program while exiting
+
+// console.log("welcome to the slient auction")
+// let name = prompt("what is your name?: ");
+// console.log(`Hello, There ${name}`);
+
+
+// console.timeEnd('running time')
+
+// window.addEventListener('mouseover', function(e){
+//     console.log(`X: ${e.x} | Y: ${e.y}`);
+// })
+
+
+console.log(Array.from("Dinesh Tamang"));
